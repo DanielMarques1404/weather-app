47a1e71152538ed462ac2f71be9ba9607ad0cdc4
daniel <daniel.marques.bnb@gmail.com>
2026-03-03 20:05:20 -0300

    Today component

---
+++ src/components/Today.tsx
@@ -0,0 +1,44 @@
+import { cn } from "../libs/cn";
+
+type TodayProps = {
+  variant: "small" | "large";
+  temperature: number;
+  date: Date;
+  city: string;
+  weatherIcon: string;
+};
+
+export const Today = (props: TodayProps) => {
+  return (
+    <div className="relative grid font-dmSans rounded-2xl overflow-hidden">
+      <img
+        src="/assets/images/bg-today-small.svg"
+        alt=""
+        className={cn(
+          "col-start-1 -col-end-1 row-start-1 -row-end-1 object-cover w-full h-full z-0",
+          { hidden: props.variant === "large" },
+        )}
+      />
+      <img
+        src="/assets/images/bg-today-large.svg"
+        alt=""
+        className={cn(
+          "col-start-1 -col-end-1 row-start-1 -row-end-1 object-cover w-full h-full z-0",
+          { hidden: props.variant === "small" },
+        )}
+      />
+      <div className="col-start-1 -col-end-1 row-start-1 -row-end-1 z-10 flex items-center justify-center">
+        <div className="grid grid-rows-2 items-center justify-center">
+          <div className="flex flex-col gap-2">
+            <span className="text-3xl font-semibold text-Neutral-200">{props.city}</span>
+            <span>{props.date.toLocaleDateString()}</span>
+          </div>
+          <div className="flex w-full items-center justify-between">
+            <img className="w-32 h-32" src={props.weatherIcon} alt="" />
+            <span className="text-8xl font-bold italic text-Neutral-0">{props.temperature}°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
