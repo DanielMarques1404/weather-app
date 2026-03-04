09cc73b29ed645672d4c3b5d640c0e5e402da875
daniel <daniel.marques.bnb@gmail.com>
2026-03-03 22:20:51 -0300

    First contact with open-meteo-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Today } from "./components/Today";
+import { OpenMeteoApi } from "./libs/open-meteo-api";
 
 function App() {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const omApi = new OpenMeteoApi(-10.7172, -38.5431);
+      await omApi.fetchWeather();
+      setData(omApi.getCurrentData());
+    }
+
+    fetchData();
+
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (!data) {
+    return <span className="text-lg text-Neutral-0">Loading...</span>;
+  }
+
   return (
     <div className="m-auto w-80">
       <Today
         variant={"small"}
-        temperature={20}
-        date={new Date()}
+        temperature={Math.floor(data.temperature_2m)}
+        date={data.date}
         city={"Brazil, Fortaleza"}
         weatherIcon={"/assets/images/icon-sunny.webp"}
       />
---
+++ src/libs/open-meteo-api.ts
@@ -0,0 +1,98 @@
+import { fetchWeatherApi } from "openmeteo";
+
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41
+// &daily=temperature_2m_max,temperature_2m_min,weather_code
+// &hourly=temperature_2m,weather_code
+// &current=relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,temperature_2m
+// &timezone=auto  
+
+
+type openMeteoApiParams = {
+  latitude: number;
+  longitude: number;
+  hourly: string;
+  daily: string;
+  current: string;
+  timezone: string;
+};
+
+export class OpenMeteoApi {
+  private params: openMeteoApiParams;
+  private data: any;
+
+  constructor(latitude: number, longitude: number) {
+    this.params = {
+      latitude: latitude,
+      longitude: longitude,
+      hourly: "temperature_2m,weather_code",
+      daily: "temperature_2m_max,temperature_2m_min,weather_code",
+      current:
+        "relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,temperature_2m",
+      timezone: "auto",
+    };
+  }
+
+  async fetchWeather() {
+    const url = "https://api.open-meteo.com/v1/forecast";
+    const responses = await fetchWeatherApi(url, this.params);
+    this.data = responses[0];
+  }
+
+  getHourlyData() {
+    const hourly = this.data.hourly()!;
+    const utcOffsetSeconds = this.data.utcOffsetSeconds();
+    return {
+      hourly: {
+        time: Array.from(
+          {
+            length:
+              (Number(hourly.timeEnd()) - Number(hourly.time())) /
+              hourly.interval(),
+          },
+          (_, i) =>
+            new Date(
+              (Number(hourly.time()) +
+                i * hourly.interval() +
+                utcOffsetSeconds) *
+                1000,
+            ),
+        ),
+        temperature_2m: hourly.variables(0)!.valuesArray(),
+        weather_code: hourly.variables(1)!.valuesArray(),
+      },
+    };
+  }
+
+  getCurrentData() {
+    const current = this.data.current()!;
+    return {
+      relative_humidity_2m: current.variables(0)!.value(),
+      apparent_temperature: current.variables(1)!.value(),
+      precipitation: current.variables(2)!.value(),
+      wind_speed_10m: current.variables(3)!.value(),
+      temperature_2m: current.variables(4)!.value(),
+      date: new Date(),
+    };
+  }
+
+  getDailyData() {
+    const daily = this.data.daily()!;
+    const utcOffsetSeconds = this.data.utcOffsetSeconds();
+    return {
+      time: Array.from(
+        {
+          length:
+            (Number(daily.timeEnd()) - Number(daily.time())) / daily.interval(),
+        },
+        (_, i) =>
+          new Date(
+            (Number(daily.time()) + i * daily.interval() + utcOffsetSeconds) *
+              1000,
+          ),
+      ),
+      temperature_2m_max: daily.variables(0)!.valuesArray(),
+      temperature_2m_min: daily.variables(1)!.valuesArray(),
+      weather_code: daily.variables(2)!.valuesArray(),
+    };
+  }
+}
